bcbd31b0847927b6ec8f0d05517b3c313e1dba81
Rokas <rokasatminas@gmail.com>
2025-10-29 21:20:45 +0200

    allow for user input, output average hours to work per remaining days

--- WorkHoursPlanner/MainWindow.xaml.cs
+++ WorkHoursPlanner/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WorkHoursPlanner;
 
@@ -16,8 +7,31 @@ namespace WorkHoursPlanner;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private const int TYPICAL_HOURS_PER_DAY = 8;
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void OnCalculateClick(object sender, RoutedEventArgs e)
+    {
+        if (int.TryParse(RequiredFullWorkDays.Text, out var requiredFullWorkDays)
+            && int.TryParse(DaysWorked.Text, out var daysWorked)
+            && double.TryParse(HoursWorked.Text, out var hoursWorked))
+        {
+            var requiredHoursToWork = requiredFullWorkDays * TYPICAL_HOURS_PER_DAY;
+            var remainingHoursToWork = requiredHoursToWork - hoursWorked;
+            var remainingDaysToWork = requiredFullWorkDays - daysWorked;
+            var averageHoursToWorkPerRemainingDay = remainingHoursToWork / remainingDaysToWork;
+
+            var fmtString = TimeSpan.FromHours(averageHoursToWorkPerRemainingDay).ToString(@"hh\:mm");
+
+            ResultText.Text = fmtString;
+        }
+        else
+        {
+            ResultText.Text = "Please enter valid numbers for all fields.";
+        }
     }
 }
